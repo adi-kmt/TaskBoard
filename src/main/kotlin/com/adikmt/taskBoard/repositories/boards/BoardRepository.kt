495b65e9d348b0e67e5e664612f5ab20678823ef
Adithya Kamath <kamathis4@gmail.com>
2023-05-30 17:18:38 +0530

    Boards repository impl

---
+++ src/main/kotlin/com/adikmt/taskBoard/repositories/boards/BoardRepository.kt
@@ -0,0 +1,19 @@
+package com.adikmt.taskBoard.repositories.boards
+
+import com.adikmt.taskBoard.dtos.common.DbResponseWrapper
+import com.adikmt.taskBoard.dtos.requests.BoardRequest
+import jooq.generated.tables.records.BoardsRecord
+
+interface BoardRepository {
+    /**
+     * APIs to implement (Only as applicable to particular user)
+     * 1. Create Board
+     * 2. Get Board by ID
+     * 3. Search by board by name
+     * 4. Get All Boards
+     */
+    fun createBoard(boardRequest: BoardRequest, userId: Int): DbResponseWrapper<Int>
+    fun getBoardById(boardId: Int, userId: Int): DbResponseWrapper<BoardsRecord?>
+    fun searchBoardByName(boardName: String, userId: Int): DbResponseWrapper<List<BoardsRecord>?>
+    fun getAllBoardsForUser(userId: Int): DbResponseWrapper<List<BoardsRecord>?>
+}
