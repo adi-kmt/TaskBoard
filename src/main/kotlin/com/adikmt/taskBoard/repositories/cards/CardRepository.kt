de3c32dd14ee2f6b81af66a38644fe3da52b2b34
Adithya Kamath <kamathis4@gmail.com>
2023-06-04 22:25:11 +0530

    Cards Repos added

--- src/main/kotlin/com/adikmt/taskBoard/repositories/cards/CardRepository.kt
+++ src/main/kotlin/com/adikmt/taskBoard/repositories/cards/CardRepository.kt
@@ -1,10 +1,11 @@
 package com.adikmt.taskBoard.repositories.cards
 
+import com.adikmt.taskBoard.dtos.common.wrappers.DbResponseWrapper
 import com.adikmt.taskBoard.dtos.requests.CardRequest
 import com.adikmt.taskBoard.dtos.requests.CardUpdateBucketRequest
 import com.adikmt.taskBoard.dtos.requests.CardUpdateRequest
 import com.adikmt.taskBoard.dtos.requests.CardUpdateUserRequest
-import jooq.generated.tables.records.CardsRecord
+import com.adikmt.taskBoard.dtos.responses.CardResponse
 import kotlinx.coroutines.flow.Flow
 
 interface CardRepository {
@@ -24,10 +25,10 @@ interface CardRepository {
      *
      * Cannot delete
      */
-    fun createCard(cardRequest: CardRequest, userId: Int): DbResponseWrapper<Int?>
-    fun getAllCards(boardId: Int): Flow<DbResponseWrapper<CardsRecord>>
-    fun getAllCardsAssignedToUserById(userId: Int, boardId: Int): Flow<DbResponseWrapper<CardsRecord>>
-    fun updateCardDetails(cardRequest: CardUpdateRequest): DbResponseWrapper<Int>
-    fun updateCardBucket(cardUpdateBucketRequest: CardUpdateBucketRequest): DbResponseWrapper<Int>
-    fun assignCardToAnotherUser(cardUpdateUserRequest: CardUpdateUserRequest): DbResponseWrapper<Int>
+    fun createCard(cardRequest: CardRequest, userId: Int): DbResponseWrapper<out Int>
+    fun getAllCards(boardId: Int): Flow<DbResponseWrapper<CardResponse>>
+    fun getAllCardsAssignedToUserById(userId: Int, boardId: Int): Flow<DbResponseWrapper<CardResponse>>
+    fun updateCardDetails(cardRequest: CardUpdateRequest): DbResponseWrapper<out Int>
+    fun updateCardBucket(cardUpdateBucketRequest: CardUpdateBucketRequest): DbResponseWrapper<out Int>
+    fun assignCardToAnotherUser(cardUpdateUserRequest: CardUpdateUserRequest): DbResponseWrapper<out Int>
 }
--- src/main/kotlin/com/adikmt/taskBoard/repositories/cards/CardRepositoryImpl.kt
+++ src/main/kotlin/com/adikmt/taskBoard/repositories/cards/CardRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.adikmt.taskBoard.repositories.cards
 
+import com.adikmt.taskBoard.dtos.common.mappers.toCardResponse
+import com.adikmt.taskBoard.dtos.common.wrappers.DbResponseWrapper
 import com.adikmt.taskBoard.dtos.requests.CardRequest
 import com.adikmt.taskBoard.dtos.requests.CardUpdateBucketRequest
 import com.adikmt.taskBoard.dtos.requests.CardUpdateRequest
 import com.adikmt.taskBoard.dtos.requests.CardUpdateUserRequest
+import com.adikmt.taskBoard.dtos.responses.CardResponse
 import java.time.LocalDateTime
 import jooq.generated.tables.records.CardsRecord
 import jooq.generated.tables.references.BOARDS
@@ -17,7 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import reactor.core.publisher.Flux
 
 class CardRepositoryImpl(@Autowired private val context: DSLContext) : CardRepository {
-    override fun createCard(cardRequest: CardRequest, userId: Int): DbResponseWrapper<Int?> {
+    override fun createCard(cardRequest: CardRequest, userId: Int): DbResponseWrapper<out Int> {
         return try {
             val cardId = context.insertInto<CardsRecord>(CARDS)
                 .set(CARDS.CARD_TITLE, cardRequest.title)
@@ -27,23 +30,23 @@ class CardRepositoryImpl(@Autowired private val context: DSLContext) : CardRepos
                 .set(CARDS.USER_ASSIGNED_ID, userId)
                 .set(CARDS.CARD_START_DATE, LocalDateTime.parse(cardRequest.startDate))
                 .set(CARDS.CARD_END_DATE, LocalDateTime.parse(cardRequest.endDate))
-                ?.onDuplicateKeyIgnore()
-                ?.returningResult<Int>(CARDS.ID)
-                ?.execute()
+                .onDuplicateKeyIgnore()
+                .returningResult<Int>(CARDS.ID)
+                .execute()
 
-            DbResponseWrapper<Int?>(
+            DbResponseWrapper.Success(
                 data = cardId
             )
         } catch (e: Exception) {
-            DbResponseWrapper(
+            DbResponseWrapper.ServerException(
                 exception = e
             )
         }
     }
 
-    override fun getAllCards(boardId: Int): Flow<DbResponseWrapper<CardsRecord>> {
+    override fun getAllCards(boardId: Int): Flow<DbResponseWrapper<CardResponse>> {
         return try {
-            Flux.from<DbResponseWrapper<CardsRecord>> {
+            Flux.from<DbResponseWrapper<CardResponse>> {
                 context.select()
                     .from(BOARDS)
                     .innerJoin(BUCKETS).on(BOARDS.ID.eq(BUCKETS.BOARD_ID))
@@ -51,15 +54,18 @@ class CardRepositoryImpl(@Autowired private val context: DSLContext) : CardRepos
                     .where(BOARDS.ID.eq(boardId).and(CARDS.IS_CARD_ARCHIVED.isFalse))
                     .orderBy(CARDS.CARD_END_DATE)
                     .fetchStreamInto(CardsRecord::class.java)
+                    .map {
+                        it.toCardResponse()
+                    }
                     .map { card ->
-                        DbResponseWrapper(data = card)
+                        DbResponseWrapper.Success(data = card)
                     }
             }
                 .distinctUntilChanged()
                 .asFlow()
         } catch (e: Exception) {
             flow {
-                DbResponseWrapper<CardsRecord>(
+                DbResponseWrapper.ServerException(
                     exception = e
                 )
             }
@@ -69,9 +75,9 @@ class CardRepositoryImpl(@Autowired private val context: DSLContext) : CardRepos
     override fun getAllCardsAssignedToUserById(
         userId: Int,
         boardId: Int
-    ): Flow<DbResponseWrapper<CardsRecord>> {
+    ): Flow<DbResponseWrapper<CardResponse>> {
         return try {
-            Flux.from<DbResponseWrapper<CardsRecord>> {
+            Flux.from<DbResponseWrapper<CardResponse>> {
                 context.select()
                     .from(BOARDS)
                     .innerJoin(BUCKETS).on(BOARDS.ID.eq(BUCKETS.BOARD_ID))
@@ -83,22 +89,25 @@ class CardRepositoryImpl(@Autowired private val context: DSLContext) : CardRepos
                     )
                     .orderBy(CARDS.CARD_END_DATE)
                     .fetchStreamInto(CardsRecord::class.java)
+                    .map {
+                        it.toCardResponse()
+                    }
                     .map { card ->
-                        DbResponseWrapper(data = card)
+                        DbResponseWrapper.Success(data = card)
                     }
             }
                 .distinctUntilChanged()
                 .asFlow()
         } catch (e: Exception) {
             flow {
-                DbResponseWrapper<CardsRecord>(
+                DbResponseWrapper.ServerException(
                     exception = e
                 )
             }
         }
     }
 
-    override fun updateCardDetails(cardRequest: CardUpdateRequest): DbResponseWrapper<Int> {
+    override fun updateCardDetails(cardRequest: CardUpdateRequest): DbResponseWrapper<out Int> {
         return try {
             val id = context.update(CARDS)
                 .set(CARDS.IS_CARD_ARCHIVED, cardRequest.isCardArchived)
@@ -108,41 +117,41 @@ class CardRepositoryImpl(@Autowired private val context: DSLContext) : CardRepos
                 .returningResult<Int>(CARDS.ID)
                 .execute()
 
-            DbResponseWrapper(data = id)
+            DbResponseWrapper.Success(data = id)
         } catch (e: Exception) {
-            DbResponseWrapper(
+            DbResponseWrapper.ServerException(
                 exception = e
             )
         }
     }
 
-    override fun updateCardBucket(cardRequest: CardUpdateBucketRequest): DbResponseWrapper<Int> {
+    override fun updateCardBucket(cardUpdateBucketRequest: CardUpdateBucketRequest): DbResponseWrapper<out Int> {
         return try {
             val id = context.update(CARDS)
-                .set(CARDS.BUCKET_ID, cardRequest.bucketId)
-                .where(CARDS.ID.eq(cardRequest.id))
+                .set(CARDS.BUCKET_ID, cardUpdateBucketRequest.bucketId)
+                .where(CARDS.ID.eq(cardUpdateBucketRequest.id))
                 .returningResult<Int>(CARDS.ID)
                 .execute()
 
-            DbResponseWrapper(data = id)
+            DbResponseWrapper.Success(data = id)
         } catch (e: Exception) {
-            DbResponseWrapper(
+            DbResponseWrapper.ServerException(
                 exception = e
             )
         }
     }
 
-    override fun assignCardToAnotherUser(cardRequest: CardUpdateUserRequest): DbResponseWrapper<Int> {
+    override fun assignCardToAnotherUser(cardUpdateUserRequest: CardUpdateUserRequest): DbResponseWrapper<out Int> {
         return try {
             val id = context.update(CARDS)
-                .set(CARDS.USER_ASSIGNED_ID, cardRequest.userId)
-                .where(CARDS.ID.eq(cardRequest.id))
+                .set(CARDS.USER_ASSIGNED_ID, cardUpdateUserRequest.newUserId)
+                .where(CARDS.ID.eq(cardUpdateUserRequest.id))
                 .returningResult<Int>(CARDS.ID)
                 .execute()
 
-            DbResponseWrapper(data = id)
+            DbResponseWrapper.Success(data = id)
         } catch (e: Exception) {
-            DbResponseWrapper(
+            DbResponseWrapper.ServerException(
                 exception = e
             )
         }
